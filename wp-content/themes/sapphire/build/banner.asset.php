69ad133a7996d5f7b9973ce545f5c39cffc84217
Bobby Lee <bobby@bobbylee.dev>
2022-05-22 19:23:50 -0700

    feat: Sapphire theme added

---
+++ wp-content/themes/sapphire/build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => '078ef6b9f88edfafd667');
---
+++ wp-content/themes/sapphire/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '391159d7a1a98a956120');
